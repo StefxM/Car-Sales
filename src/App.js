9a190c744227328085ee587e98b557f915dcc833
stephanie mairena <stefyem7711@gmail.com>
2020-06-23 00:49:07 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -4,37 +4,36 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from 'react-redux';
+import { remove_features, add_features } from './components/actions/actions';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+const App = (props) => {
+  
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} remove_features={props.remove_features} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} add_features={props.add_features} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
+//mapping everything we have in state and connecting it to props
+function mapStateToProps(state) {
 
-export default App;
+
+  return{
+        additionalPrice: state.additionalPrice,
+        car: state.car,//price,name,image,and features bundled in car
+        additionalFeatures: state.additionalFeatures
+  }
+}
+
+
+                      //connecting the store to the (App) component to have access to State
+export default connect(mapStateToProps,{remove_features, add_features})(App);
--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,10 +1,12 @@
 import React from 'react';
+//no longer needed to import since we are passing this down as props
+//import { remove_features } from './actions/actions';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.remove_features(props.feature.id)}>X</button>
       {props.feature.name}
     </li>
   );
